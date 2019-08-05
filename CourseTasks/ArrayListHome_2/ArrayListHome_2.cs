a262cdfdf2d17e6240b820799281d26dc3c291a7
Константин <keks946@gmail.com>
2019-08-05 21:28:19 +0700

    готова ArrayListHome

---
+++ CourseTasks/ArrayListHome_2/ArrayListHome_2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListHome_2
+{
+    class ArrayListHome_2
+    {
+        static void Main(string[] args)
+        {
+            List<int> myIntList = new List<int> { 5, 46, 32, 17, 112, 543, 97, 57, 66 };
+            int i = 0;
+            
+            while (i < myIntList.Count)
+            {
+                if (myIntList[i] % 2 == 0)
+                {
+                    myIntList.RemoveAt(i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            foreach (int e in myIntList)
+            {
+                Console.Write(e + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CourseTasks/ArrayListHome_3/ArrayListHome_3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListHome_3
+{
+    class ArrayListHome_3
+    {
+        static void Main(string[] args)
+        {
+            List<int> myIntList = new List<int> { 5, 46, 32, 17, 112, 5, 66, 543, 97, 112, 57, 66 };
+            List<int> myNewIntList = new List<int>();
+            for (int i = 0; i < myIntList.Count; i++)
+            {
+                if (!myNewIntList.Contains(myIntList[i]))
+                {
+                    myNewIntList.Add(myIntList[i]);
+                }
+            }
+
+            foreach (int e in myNewIntList)
+            {
+                Console.Write(e + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
