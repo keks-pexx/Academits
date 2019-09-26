92a5f1b362adb74840682ccc85a4564d75e12eb4
Константин <keks946@gmail.com>
2019-09-26 21:05:00 +0700

    Выполнена HashTable

--- CourseTasks/HashTableTask/HashTableTask.cs
+++ CourseTasks/HashTableTask/HashTableTask.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HashTableTask
 {
@@ -10,6 +6,25 @@ namespace HashTableTask
     {
         static void Main(string[] args)
         {
-        }
+            var hashTable = new MyHashTable<string>(20);
+
+            hashTable.Add("Значение1");
+            hashTable.Add("Значение3");
+            hashTable.Add("Значение 123");
+            hashTable.Add("200");
+            hashTable.Add("345");
+            hashTable.Add(null);
+            hashTable.Add("Nubmer2");           
+
+            Console.WriteLine(hashTable);
+            Console.WriteLine(hashTable.Contains("345"));            
+            Console.WriteLine();
+
+            hashTable.Remove("Значение3");
+            hashTable.Remove(null);
+            Console.WriteLine(hashTable);
+
+            Console.ReadKey();
+        }             
     }
 }
--- CourseTasks/HashTableTask/MyHashTable.cs
+++ CourseTasks/HashTableTask/MyHashTable.cs
@@ -1,23 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HashTableTask
 {
     class MyHashTable<T> : ICollection<T>
     {
-        private List<T>[] items = new List<T>[100];
-        private int modCount = 0;
-        private const int NullIndex = 0;
+        private List<T>[] contents = new List<T>[100];
+        private int modCount = 0;   
         public int Count { get; private set; }
-
-        public bool IsReadOnly
-        {
-            get { return false; }
-        }
 
         public MyHashTable(params T[] data)
         {
@@ -33,28 +25,100 @@ namespace HashTableTask
         {
             if (capacity < 0)
             {
-                throw new ArgumentOutOfRangeException("Размерность массива не может быть меньше 0");
+                throw new ArgumentOutOfRangeException("размер не может быть меньше 0");
             }
 
-            items = new List<T>[capacity];
+            contents = new List<T>[capacity];
             Count = 0;
         }
 
-        public IEnumerator<T> GetEnumerator()
+        public bool IsReadOnly
         {
-            var startModCount = modCount;
-            foreach (var t in items)
+            get { return false; }
+        }
+
+        public void Add(T item)
+        {
+            var index = GetIndex(item);
+
+            if (ReferenceEquals(contents[index], null))
+            {
+                contents[index] = new List<T>();
+            }
+
+            contents[index].Add(item);
+
+            Count++;
+            modCount++;
+        }
+
+        public void Clear()
+        {
+            foreach (List<T> t in contents)
             {
                 if (ReferenceEquals(t, null))
                 {
                     continue;
                 }
 
-                foreach (var e in t)
+                t.Clear();
+            }
+
+            Count = 0;
+            modCount++;
+        }
+
+        public bool Contains(T item)
+        {
+            var index = GetIndex(item);
+
+            if (ReferenceEquals(contents[index], null) || contents[index].Count == 0)
+            {
+                return false;
+            }
+
+            return contents[index].Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (ReferenceEquals(array, null))
+            {
+                throw new ArgumentNullException("cсылка на null");
+            }
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException("индекс меньше 0");
+            }
+            if (Count > array.Length - arrayIndex)
+            {
+                throw new ArgumentException("недостаточно места для копируемого массива");
+            }
+
+            var i = arrayIndex;
+
+            foreach (var e in this)
+            {
+                array[i] = e;
+                i++;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int modCount = this.modCount;
+            foreach (var item in contents)
+            {
+                if (ReferenceEquals(item, null))
                 {
-                    if (modCount != startModCount)
+                    continue;
+                }
+
+                foreach (var e in item)
+                {
+                    if (modCount != this.modCount)
                     {
-                        throw new InvalidOperationException("Список изменился за время обхода");
+                        throw new InvalidOperationException("Список был изменен");
                     }
 
                     yield return e;
@@ -67,86 +131,19 @@ namespace HashTableTask
             return GetEnumerator();
         }
 
-        public void Add(T item)
-        {
-            var index = GetIndex(item);
-
-            if (ReferenceEquals(items[index], null))
-            {
-                items[index] = new List<T>();
-            }
-
-            items[index].Add(item);
-
-            ++Count;
-            ++modCount;
-        }
-
-        public void Clear()
-        {
-            foreach (var t in items)
-            {
-                if (ReferenceEquals(t, null))
-                {
-                    continue;
-                }
-
-                t.Clear();
-            }
-
-            Count = 0;
-            ++modCount;
-        }
-
-        public bool Contains(T item)
-        {
-            var index = GetIndex(item);
-
-            if (ReferenceEquals(items[index], null) || items[index].Count == 0)
-            {
-                return false;
-            }
-
-            return items[index].Contains(item);
-        }
-
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            if (ReferenceEquals(array, null))
-            {
-                throw new ArgumentNullException("Ссылка на массив null");
-            }
-            if (arrayIndex < 0)
-            {
-                throw new ArgumentOutOfRangeException("индекс меньше 0");
-            }
-            if (Count > array.Length - arrayIndex)
-            {
-                throw new ArgumentException("передаваемый массив больше доступного места в целевом массиве");
-            }
-
-            var i = arrayIndex;
-
-            foreach (var e in this)
-            {
-                array[i] = e;
-                i++;
-            }
-        }
-
         public bool Remove(T item)
         {
             var index = GetIndex(item);
 
-            if (ReferenceEquals(items[index], null))
+            if (ReferenceEquals(contents[index], null))
             {
                 return false;
             }
 
-            if (items[index].Remove(item))
+            if (contents[index].Remove(item))
             {
-                --Count;
-                ++modCount;
+                Count--;
+                modCount++;
 
                 return true;
             }
@@ -156,7 +153,34 @@ namespace HashTableTask
 
         private int GetIndex(T item)
         {
-            return ReferenceEquals(item, null) ? NullIndex : Math.Abs(item.GetHashCode() % items.Length);
+            return ReferenceEquals(item, null) ? 0 : Math.Abs(item.GetHashCode() % contents.Length);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            var hashTable = this;
+
+            sb.Append("{ ");
+
+            foreach (var e in hashTable)
+            {                
+                if (e == null)
+                {
+                    sb.Append("null");
+                }
+                else
+                {
+                    sb.Append(e);
+                }
+
+                sb.Append(", ");
+            }
+
+            sb.Remove(sb.Length - 2, 2);
+            sb.Append(" }");
+
+            return sb.ToString();
         }
     }
 }
