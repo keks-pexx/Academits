7a22cb226fb7cf2190d64c0da85471eff62ba25e
Константин <keks946@gmail.com>
2019-10-07 21:03:08 +0700

    Поправил HashTableTask

--- CourseTasks/HashTableTask/HashTableTask.cs
+++ CourseTasks/HashTableTask/HashTableTask.cs
@@ -5,9 +5,10 @@ namespace HashTableTask
     class HashTableTask
     {
         static void Main(string[] args)
-        {
+        {          
             var hashTable = new MyHashTable<string>(20);
 
+            
             hashTable.Add("Значение1");
             hashTable.Add("Значение3");
             hashTable.Add("Значение 123");
@@ -15,15 +16,17 @@ namespace HashTableTask
             hashTable.Add("345");
             hashTable.Add(null);
             hashTable.Add("Nubmer2");           
+            
 
             Console.WriteLine(hashTable);
+            
             Console.WriteLine(hashTable.Contains("345"));            
             Console.WriteLine();
 
             hashTable.Remove("Значение3");
             hashTable.Remove(null);
             Console.WriteLine(hashTable);
-
+            
             Console.ReadKey();
         }             
     }
